7c3d1a4e706f9df667fa82b5a67c9589cad2c1b3
xunlei98 <xunlei98@gmail.com>
2016-12-18 22:33:46 +0800

    Stephen Grider (latest) - 2nd Project
    From my MacBook

---
+++ index.ios.js
@@ -0,0 +1,43 @@
+// Import some code we need
+var Moment = require('moment');
+var React = require('react-native');
+var AppRegistry = React.AppRegistry;
+var Text = React.Text;
+var View = React.View;
+var StyleSheet = React.StyleSheet;
+var DayItem = require('./src/day-item');
+
+// Create a react component
+var Weekdays = React.createClass({
+  render: function() {
+    return <View style={styles.container}>
+      {this.days()}
+    </View>
+  },
+  days: function() {
+    var daysItems = [];
+
+    for (var i = 0; i < 7; i++) {
+      var day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+    return daysItems
+  }
+});
+
+// Style the React component
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center', // Moves up and down
+    alignItems: 'center' // Moves sideway
+  }
+});
+
+// Show the react component on the screen
+AppRegistry.registerComponent('weekdays', function() {
+  return Weekdays
+});
